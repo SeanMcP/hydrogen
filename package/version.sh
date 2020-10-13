de614fc8fa4dae4e0f37a08042848545c9d1f71e
Sean McPherson <smcpherson@niche.com>
2020-10-13 14:10:15 -0400

    chore: patch v0.0.1

---
+++ package/version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+next_version=$(npm version $@)
+
+# Add all changes in the repo
+git add -A
+
+# Commit changes
+git commit -m "chore: $1 $next_version"
+
+# Create lightweight tag
+git tag "$next_version"
+
+git push
+git push --tags
